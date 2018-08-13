c41ad81e07e52e7c5629a78ebc5fc8945986efb2
Ariana Baseman <ArianaMB@yahoo.com>
2018-08-13 19:34:20 -0400

    JavaScript_Game_Lab_Part2

--- JavaScript_Game_Lab_1.js
+++ JavaScript_Game_Lab_1.js
@@ -1,30 +1,44 @@
-const ask = prompt ("Do you want to play?");
-let userHealth = 40;
-let grantHealth = 10;
-const grant = "The Almighty Grant";
-let wins = 0;
+"use strict";
+{
+  const startGame = prompt ("Do you want to play?");
+  let userHealth = 40;
+  let grantHealth = 10;
+  const grant = "Grant the Mighty Chicken";
+  let wins = 0;
+ 
+  if (startGame === "yes") {
+    const name = prompt("What is your name?");{
+    startCombat();
+  }
+    function startCombat () {
+      let userWon = (`${name} wins!!!!!!!!!!`);
+        while (userHealth > 0 && wins < 3) {
+         grantHealth-=getDamage(1, 5);
+         userHealth-=getDamage(1, 5);
+           const result = `${grant} has ${grantHealth} left. ${name} has ${userHealth} left.`;
+           const action = prompt("attack or quit? (enter 'a' or 'q' for your choice)")
+            if (action !== "a") {
+                console.log ("Thank you for playing today!");
+                break;
+             } 
+            console.log (result);
+            if (grantHealth <= 0) {
+              grantHealth = 10;
+              wins++;
+            }
+            if (wins === 3) {
+               console.log (userWon);
+              }
+           }
+            if (userHealth < 0 && wins <3) {
+            console.log(`${grant} won!!`);
+            }
+          }
+}
+    function getDamage(min, max) {
+      return Math.floor(min + Math.random()*(max+1 - min))
 
-
-
-if (ask === "yes") {
-  const name = ("What is your name?");
-  const userName = prompt(name);
-  let userWon = (`${userName} wins!!!!!!!!!!`);
-while (userHealth > 0 && wins < 3) {
-  grantHealth-=genRanInt(1, 2);
-  userHealth-=genRanInt(1, 2);
-  const result = `${grant} has ${grantHealth} left. ${userName} has ${userHealth} left.`;
-  console.log (result)
-    if (grantHealth <= 0) {
-      grantHealth = 10;
-      wins++;
-      if (wins === 3) {
-        console.log (userWon);
       }
 }
-function genRanInt(min, max) {
-  return Math.floor(min + Math.random()*(max+1 - min))
-}
-}
-}
+  
 
